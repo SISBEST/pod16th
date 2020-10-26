9550c12f2a2c30a169e166151c71d7c4d3a990ff
Samuel Sharp <samuelsharp4@gmail.com>
2020-10-26 11:41:40 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+#app {
+  font-family: serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #62361b;
+}
+
+h1 {
+  color: #f9e300;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="home">
+    <h1>POD16th</h1>
+    <p>Platinum Sponsor:</p>
+    <img src="/img/falling.png" alt="I never loved you!" style="height:120px" />
+  </div>
+</template>
